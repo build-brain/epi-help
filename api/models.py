a345ddc8c2451ab83f5da39f91a4bf30096fbc9a
Jasurbek-Tursunov <jasurbek3124@icloud.com>
2022-10-16 20:51:50 +0500

    Restyle -> api/models.py

--- api/models.py
+++ api/models.py
@@ -1,38 +1,71 @@
+"""
+    Here is the code of models from the database
+    --------------------------------------------
+    Link https://github.com/build-brain/epi-help/blob/main/api/models.py
+"""
+
+
+
 from django.db import models
 from datetime import date, time
 
 
+
+# -------------- Type Trigger -------------------
+
 class TypeTrigger(models.Model):
     """ """
-    name = models.CharField(max_length=150)
+    name     = models.CharField(max_length=150)
     selected = models.BooleanField(default=False)
 
     class Meta:
-        verbose_name = "Type Trigger"
+        verbose_name        = "Type Trigger"
         verbose_name_plural = "Types Trigger"
 
     def __str__(self):
         return str(self.name)
 
 
+
+# -------------- Type Seizure -------------------
+
 class TypeSeizure(models.Model):
     """ """
-    name = models.CharField(max_length=150)
+    name         = models.CharField(max_length=150)
     descriptions = models.TextField()
 
     class Meta:
-        verbose_name = "Type Seizure"
+        verbose_name        = "Type Seizure"
         verbose_name_plural = "Types Seizure"
 
     def __str__(self):
         return str(self.name)
 
 
+
+# -------------- Type Aura ----------------------
+
+class TypeAura(models.Model):
+    """ """
+    name         = models.CharField(max_length=150)
+    descriptions = models.TextField()
+
+    class Meta:
+        verbose_name        = "Type Aura"
+        verbose_name_plural = "Types Aura"
+
+    def __str__(self):
+        return str(self.name)
+
+
+
+# --------------- Seizure -----------------------
+
 class Seizure(models.Model):
     """ """
-    when = models.DateTimeField()
+    when     = models.DateTimeField()
     duration = models.DurationField()
-    trigger = models.CharField(max_length=150)
+    trigger  = models.CharField(max_length=150)
     descriptions_trigger = models.TextField()
     type = models.OneToOneField(
         TypeSeizure, 
@@ -42,31 +75,21 @@ class Seizure(models.Model):
     comment = models.TextField()
 
     class Meta:
-        verbose_name = "Seizure"
+        verbose_name        = "Seizure"
         verbose_name_plural = "Seizure"
 
     def __str__(self):
         return str(self.duration)
 
 
-class TypeAura(models.Model):
-    """ """
-    name = models.CharField(max_length=150)
-    descriptions = models.TextField()
 
-    class Meta:
-        verbose_name = "Type Aura"
-        verbose_name_plural = "Types Aura"
-
-    def __str__(self):
-        return str(self.name)
-
+# --------------- Aura --------------------------
 
 class Aura(models.Model):
     """ """
-    when = models.DateTimeField()
+    when     = models.DateTimeField()
     duration = models.DurationField()
-    trigger = models.CharField(max_length=150)
+    trigger  = models.CharField(max_length=150)
     descriptions_trigger = models.TextField()
     type = models.ForeignKey(
         TypeAura,
@@ -76,18 +99,21 @@ class Aura(models.Model):
     comment = models.TextField()
 
     class Meta:
-        verbose_name = "Aura"
+        verbose_name        = "Aura"
         verbose_name_plural = "Aura"
 
     def __str__(self):
         return str(self.duration)
 
 
+
+# --------------- Report Event ------------------
+
 class ReportEvent(models.Model):
     """ """
-    when = models.DateTimeField(auto_now_add = True)
+    when     = models.DateTimeField(auto_now_add = True)
     duration = models.DurationField()
-    trigger = models.ForeignKey(
+    trigger  = models.ForeignKey(
         TypeTrigger,
         on_delete = models.CASCADE,
         primary_key=True
@@ -100,39 +126,46 @@ class ReportEvent(models.Model):
     comment = models.TextField()
 
     class Meta:
-        verbose_name = "Report Event"
+        verbose_name        = "Report Event"
         verbose_name_plural = "Report Events"
 
     def __str__(self):
         return str(self.duration)
 
 
+
+# --------------- Content -----------------------
+
 class Content(models.Model):
     """ """
-    header = models.CharField(max_length=150)
-    text = models.TextField()
-    date = models.DateField(default=date.today)
-    image = models.ImageField(upload_to="content_image/")
+    header = models.CharField(max_length = 150)
+    text   = models.TextField()
+    date   = models.DateField(default = date.today)
+    image  = models.ImageField(upload_to = "content_image/")
 
     class Meta:
-        verbose_name = "Content"
+        verbose_name        = "Content"
         verbose_name_plural = "Contents"
 
     def __str__(self):
         return str(self.header)
 
 
+
+# --------------- Contact -----------------------
+
 class Contact(models.Model):
     """ """
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name      = models.CharField(max_length=100)
+    last_name       = models.CharField(max_length=100)
     disable_contact = models.BooleanField(default=False)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=20)
+    email           = models.EmailField()
+    phone_number    = models.CharField(max_length=20)
+    alert_methods   = models.ForeignKey(to, on_delete)
 
 
     class Meta:
-        verbose_name = "Contact"
+        verbose_name        = "Contact"
         verbose_name_plural = "Contacts"
 
     def __str__(self):
@@ -140,54 +173,52 @@ class Contact(models.Model):
 
 
 
-
-
-
-# TODO ---------- Gen Settings -------------------
+# ------------- General Settings ---------------
 
 class GeneralSettings(models.Model):
     """ """
-    help_delay  = models.IntegerField()
+    help_delay        = models.IntegerField()
     phone_sensitivity = models.IntegerField()
     watch_sensitivity = models.IntegerField()
-    above_beats = models.IntegerField()
-    bellow_beats = models.IntegerField()
-    audible_alarm = models.BooleanField(default=True)
-    repeat_alarm = models.IntegerField()
-    audible_message = models.BooleanField(default=True)
-    repeat_message = models.IntegerField()
-
+    above_beats       = models.IntegerField()
+    bellow_beats      = models.IntegerField()
+    audible_alarm     = models.BooleanField(default=True)
+    repeat_alarm      = models.IntegerField()
+    audible_message   = models.BooleanField(default=True)
+    repeat_message    = models.IntegerField()
 
     class Meta:
-        verbose_name = "General Settings"
+        verbose_name        = "General Settings"
         verbose_name_plural = "General Settings"
 
     def __str__(self):
         return str(f"{self.above_beats} -> {self.bellow_beats}" )
 
-# TODO ---------- My Profile -------------------
+
+
+# --------------- My Profile -------------------
 
 class MyProfile(models.Model):
     """ """
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=20)
-    birthday = models.DateField()
-    weight = models.IntegerField()
-    blood_type = models.IntegerField()
-    allergy = models.CharField(max_length=200)
+    first_name          = models.CharField(max_length=100)
+    last_name           = models.CharField(max_length=100)
+    email               = models.EmailField()
+    phone_number        = models.CharField(max_length=20)
+    birthday            = models.DateField()
+    weight              = models.IntegerField()
+    blood_type          = models.IntegerField()
+    allergy             = models.CharField(max_length=200)
     congenital_diseases = models.CharField(max_length=200)
-    acquired_diseases = models.CharField(max_length=200)
+    acquired_diseases   = models.CharField(max_length=200)
     triggers = models.ForeignKey(
         TypeTrigger,
-        verbose_name= "Triggers that precede seizures",
-        on_delete = models.CASCADE,
-        primary_key=True
+        verbose_name = "Triggers that precede seizures",
+        on_delete    = models.CASCADE,
+        primary_key  = True
     )
     
     class Meta:
-        verbose_name = "My Profile"
+        verbose_name        = "My Profile"
         verbose_name_plural = "My Profiles"
 
     def __str__(self):
